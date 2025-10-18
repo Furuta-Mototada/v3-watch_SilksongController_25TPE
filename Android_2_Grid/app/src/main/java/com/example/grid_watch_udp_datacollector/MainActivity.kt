30b5c4de5dd36dbc33c8087019c3e3b1341b2f1d
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2025-10-19 01:30:29 +0800

    feat: Grid_Watch_UDP_DataCollector

---
+++ Android_2_Grid/app/src/main/java/com/example/grid_watch_udp_datacollector/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.grid_watch_udp_datacollector
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.grid_watch_udp_datacollector.ui.theme.Grid_Watch_UDP_DataCollectorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Grid_Watch_UDP_DataCollectorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Grid_Watch_UDP_DataCollectorTheme {
+        Greeting("Android")
+    }
+}
